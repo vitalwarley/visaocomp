b540dc742c552ae2ea41f8afa5e869be268394d2
vitalwarley <wvb@ic.ufal.br>
2018-07-15 09:04:41 -0300

    fix rectangle coordinates

--- aulas/04_1007/01.py
+++ aulas/04_1007/01.py
@@ -6,14 +6,19 @@ Created by lativ on 10/07/18 at 11:03
 
 import cv2
 
-path_img = "/home/lativ/Documents/UFAL/repos/visaocomp/img/lg.png"
-rgb = cv2.imread(path_img, cv2.IMREAD_COLOR)
+# String contendo o caminho para as imagens usadas no projeto
+folder_imgs = "/home/lativ/Documents/UFAL/repos/visaocomp/img/"
+# Lendo 'lg.png' como imagem colorida
+rgb = cv2.imread(folder_imgs + 'Lenna.png', cv2.IMREAD_COLOR)
+# Convertendo a imagem lida acima para escala de cinza
 gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+# Partes principais da detecção de faces
 cascade_xml = '/usr/local/opencv/src/opencv-3.4.1/data/haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(cascade_xml)
 faces = face_cascade.detectMultiScale(gray, 1.2, 5)
 for (x, y, w, h) in faces:
-    cv2.rectangle(rgb, (x, y), (w, h), (255, 0, 0), 2)
+    cv2.rectangle(rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 cv2.imshow('rgb', rgb)
 while True:
---
+++ revisao/04_1007/01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created by lativ on 15/07/18 at 07:23
+"""
